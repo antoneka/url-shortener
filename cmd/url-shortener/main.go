682da9f1805ef397275b699e97aa7bfa65098213
antoneka <ku-9a@mail.ru>
2024-02-20 23:09:38 +0300

    feat(config): add config/local.yaml && config.go

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,7 +1,15 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/antoneka/url-shortener/internal/config"
+)
+
 func main() {
-	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+
+	fmt.Printf("%+v\n", cfg)
 
 	// TODO: init logger: slog
 
---
+++ internal/config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeOut time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() *Config {
+	path := fetchConfigPath()
+	if path == "" {
+		panic("config path is empty")
+	}
+
+	return MustLoadByPath(path)
+}
+
+func MustLoadByPath(configPath string) *Config {
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("cannot read cpnfig: " + err.Error())
+	}
+
+	return &cfg
+}
+
+// fetchConfigPath fetches path from command line flag or environment variable.
+// Priority: flag > env > default.
+// Default value is empty string.
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "", "path to config file")
+	flag.Parse()
+
+	if res == "" {
+		res = os.Getenv("CONFIG_PATH")
+	}
+
+	return res
+}
