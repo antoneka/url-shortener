6d95ff8fb5862d2d6f01f39ddc8dc9334cc96af0
antoneka <ku-9a@mail.ru>
2024-02-22 13:08:27 +0300

    feat(storage): init sqlite

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 
 	"github.com/antoneka/url-shortener/internal/config"
+	"github.com/antoneka/url-shortener/internal/lib/logger/sl"
+	"github.com/antoneka/url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -23,7 +25,13 @@ func main() {
 	log.Info("starting url-shortener", "handler", "text")
 	log.Debug("debug messages are enabled")
 
-	// TODO: init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init router: chi, "chi render"
 
